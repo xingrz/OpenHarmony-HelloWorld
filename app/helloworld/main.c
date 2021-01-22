4cbfbd1aa8b74bdecccc5a0170f67dcb30faae55
XiNGRZ <chenxingyu92@gmail.com>
2021-01-22 23:37:13 +0800

    Hello world!
    Change-Id: Ic74ffbed85bdbbecdded49f3223b50da78292cd5

---
+++ app/helloworld/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "ohos_init.h"
+#include "ohos_types.h"
+
+void HelloWorld(void)
+{
+    printf("=========================\n");
+    printf(" 极 其 优 秀 ， 弹 性 很 足\n");
+    printf("=========================\n");
+}
+
+SYS_RUN(HelloWorld);
